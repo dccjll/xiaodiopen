f0dbfc53f2d72a6807aa606965e06af8175fe8f6
guoxiaoliang <guoxiaoliang>
2016-10-19 16:45:54 +0800

    补充提交

---
+++ BluetoothLELibrary/src/main/java/com/bluetoothle/factory/doorguard/DOORGUARDSend.java
@@ -0,0 +1,38 @@
+package com.bluetoothle.factory.doorguard;
+
+import com.bluetoothle.core.BLEConstants;
+import com.bluetoothle.core.BluetoothLeManage;
+import com.bluetoothle.core.init.BLEInit;
+import com.bluetoothle.core.writeData.OnBLEWriteDataListener;
+import com.bluetoothle.util.BLEByteUtil;
+import com.bluetoothle.util.BLELogUtil;
+
+/**
+ * Created by dessmann on 16/10/19.
+ * 门禁发送数据
+ */
+
+public class DoorGuardSend {
+    private final static String TAG = DoorGuardSend.class.getSimpleName();
+
+    /**
+     * 发送数据,不监听接收数据
+     * @param mac   目前设备mac地址
+     * @param onBLEWriteDataListener    数据发送监听器
+     */
+    public static void send(String mac, final OnBLEWriteDataListener onBLEWriteDataListener){
+        if(onBLEWriteDataListener == null){
+            BLELogUtil.e(TAG, "没有配置回调接口");
+            return;
+        }
+        byte[] data = DoorGuardProtocol.buildData();
+        BLELogUtil.e(TAG, "准备发送数据,mac=" + mac + ",data=" + BLEByteUtil.bytesToHexString(data));
+        if(BLEInit.bluetoothAdapter == null){
+            onBLEWriteDataListener.onWriteDataFail(BLEConstants.Error.CheckBluetoothAdapterError);
+            return;
+        }
+        BluetoothLeManage bluetoothLeManage = new BluetoothLeManage(BLEInit.bluetoothAdapter, mac, null, DoorGuardProtocol.buildTwoUUIDs(), null);
+        bluetoothLeManage.setData(data);
+        bluetoothLeManage.write();
+    }
+}
